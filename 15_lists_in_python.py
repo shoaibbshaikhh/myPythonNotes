8cb94dc788f6b91301c69d56265fe3ebe7f83349
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-14 18:08:08 +0530

    lists_in_python

---
+++ 15_lists_in_python.py
@@ -0,0 +1,72 @@
+# list =  used to store multiple items in a single variable
+
+food = ["pizza","vada pav","samosa","chutney"]
+
+# example1:
+# print(food)
+
+# example2:
+# food[0] = "pizza is replaced"
+# print(food)
+
+# example3:
+# for i in food:
+#     print(i)
+
+# methods in list
+# example4:
+# food.append("ice cream")
+# print(food)
+
+# example5:
+# food.remove("vada pav")
+# print(food)
+
+# example6:
+# food.pop()
+# print(food)
+
+# example7:
+# food.insert(0,"cake")
+# print(food)
+
+# example8:
+# food.sort()
+# print(food)
+
+# example9:
+# food.clear()
+# print(food)
+
+
+
+# Output:
+# example1:
+# ['pizza', 'vada pav', 'samosa', 'chutney']
+
+# example2:
+# ['pizza is replaced', 'vada pav', 'samosa', 'chutney']
+
+# example3:
+# pizza
+# vada pav
+# samosa
+# chutney
+
+# example4:
+# ['pizza', 'vada pav', 'samosa', 'chutney', 'ice cream']
+
+# example5:
+# ['pizza', 'samosa', 'chutney']
+
+# example6:
+# ['pizza', 'vada pav', 'samosa']
+
+# example7:
+# ['cake', 'pizza', 'vada pav', 'samosa', 'chutney']
+
+# example8:
+# ['chutney', 'pizza', 'samosa', 'vada pav']
+
+# example9:
+# []
