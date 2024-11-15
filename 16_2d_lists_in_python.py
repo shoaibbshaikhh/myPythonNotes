3ae48609dacd232a8eeb13db5481f6b0723994a4
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-15 09:33:22 +0530

    2d_lists_in_python

---
+++ 16_2d_lists_in_python.py
@@ -0,0 +1,18 @@
+# 2D lists = a list of lists
+
+drinks = ["coffe", "soda", "tea"]
+dinner = ["pizza", "vada pav", "samosa"]
+dessert = ["cake", "ice cream"]
+
+food = [drinks, dinner, dessert]
+
+print(food)
+print(food[0])
+print(food[1][1])
+
+
+
+# Output:
+# [['coffe', 'soda', 'tea'], ['pizza', 'vada pav', 'samosa'], ['cake', 'ice cream']]
+# ['coffe', 'soda', 'tea']
+# vada pav
