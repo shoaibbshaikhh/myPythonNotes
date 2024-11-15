2d50af75a9b1f8013d35aed3d3d9029102c858bd
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-15 17:54:50 +0530

    indexing_in_python

---
+++ 20_indexing_in_python.py
@@ -0,0 +1,35 @@
+# index operator [] = gives access to a sequence's element (str, list, tuples)
+
+name = "shoaib shaikh!"
+
+# ex1:
+# if(name[0].islower()):
+#     name = name.capitalize()
+# print(name)
+
+# ex2:
+# first_name = name[:6].upper()
+# print(first_name)
+
+# ex3:
+# last_name = name[7:].upper()
+# print(last_name)
+
+# ex4:
+# last_character = name[-1]
+# print(last_character)
+
+
+
+# Output:
+# ex1:
+# Shoaib shaikh!
+
+# ex2:
+# SHOAIB
+
+# ex3:
+# SHAIKH!
+
+# ex4:
+# !
