1ea4ae526ce7c9f6d0c601cf13a0c330c36824b9
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-16 10:47:32 +0530

    functions_in_python

---
+++ 21_functions_in_python.py
@@ -0,0 +1,16 @@
+# functions = a block of code which is executed only when it is called
+
+# ex1:
+def hello():
+    print("Hello")
+    print("Have a nice day!")
+
+hello()
+
+# ex2:
+def full_name(first_name,last_name,age):
+    print("Hello "+first_name+" "+last_name)
+    print("You are "+str(age)+" years old")
+    print("Have a nice day!")
+
+full_name("Shoaib","Shaikh",21)
