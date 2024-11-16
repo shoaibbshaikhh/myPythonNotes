e17ea12d8d68266fdaab054fef94096ea6f6020a
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-16 11:04:05 +0530

    functions_in_python_output

--- 21_functions_in_python.py
+++ 21_functions_in_python.py
@@ -14,3 +14,14 @@ def full_name(first_name,last_name,age):
     print("Have a nice day!")
 
 full_name("Shoaib","Shaikh",21)
+
+
+# Output:
+# ex1:
+# Hello
+# Have a nice day!
+#
+# ex2:
+# Hello Shoaib Shaikh
+# You are 21 years old
+# Have a nice day!
