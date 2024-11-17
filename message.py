7abaa19fa9b90f067e9c45e28168d073841bfa9e
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-17 19:14:57 +0530

    ----_in_python

--- message.py
+++ message.py
@@ -1,14 +1,6 @@
 # this python code is not run from here plz run 37_modules_in_python.py file
 
-def hello():# this python code is not run from here plz run 37_modules_in_python.py file
-
 def hello():
-    print("Hello! Have a nice day!")
-
-def bye():
-    print("Bye! Have a wonderful time :)")
-
-
     print("Hello! Have a nice day!")
 
 def bye():
