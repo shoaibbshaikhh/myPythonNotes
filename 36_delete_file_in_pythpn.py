a9f3244151e75d2ab9d8d328ea951b8a12409a8b
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-17 19:18:48 +0530

    edit_in_36_in_python

--- 36_delete_file_in_pythpn.py
+++ 36_delete_file_in_pythpn.py
@@ -3,7 +3,7 @@ import shutil
 
 path = "folder"
 try:
-    # os.remove(path) # only removes files not folder..
+    os.remove(path) # only removes files not folder..
     # os.rmdir(path)  # only removes empty directory not file
     # shutil.rmtree(path)  # delete files contain directory also but not only file
 except FileNotFoundError:
