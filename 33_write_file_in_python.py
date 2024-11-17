3d198cd71bf39464a76bdb5f7f5437c755afc673
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-17 09:34:52 +0530

    write_file_in_python

---
+++ 33_write_file_in_python.py
@@ -0,0 +1,8 @@
+text = "Yoooooo\nThis is some text\nHave a good one!\n"
+with open('test.txt','w') as file:
+    file.write(text)
+
+
+# append(add something extra) new text in file at last
+# with open('test.txt','a') as file:
+#     file.write(text)
