a2845779b15475e85db524eddc2a8838485b764b
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-17 10:46:00 +0530

    delete_file_in_python

---
+++ 36_delete_file_in_pythpn.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+path = "folder"
+try:
+    # os.remove(path) # only removes files not folder..
+    # os.rmdir(path)  # only removes empty directory not file
+    # shutil.rmtree(path)  # delete files contain directory also but not only file
+except FileNotFoundError:
+    print("That file was not found :(")
+except PermissionError:
+    print("You do not have permission to delete that :(")
+except OSError:
+    print("You cannot delete that using that function :(")
+else:
+    print(path+" was deleted :)")
