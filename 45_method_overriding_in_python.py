de9aacf490281dc531184e37ef17ffccd229b05c
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-18 06:10:15 +0530

    method_overriding_in_python

---
+++ 45_method_overriding_in_python.py
@@ -0,0 +1,16 @@
+class Animal:
+
+    def eat(self):
+        print("This animal is eating")
+
+class Rabbit(Animal):
+
+    def eat(self):
+        print("This rabbit is eating a carrot")
+
+rabbit = Rabbit()
+rabbit.eat()
+
+
+# Output:
+# This rabbit is eating a carrot
